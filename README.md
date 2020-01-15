71c97f1a5fc6f0b9ecad0e7bead891e90818322e
seleznevae <seleznevae@protonmail.com>
2020-01-15 23:17:38 +0300

    [C] Changed base phrase

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # lua-lorem
 
+**lorem** - lua library to generate [lorem ipsum](https://en.wikipedia.org/wiki/Lorem_ipsum]) (a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content).
+
 [![Build](https://github.com/seleznevae/lua-lorem/workflows/CI/badge.svg)](https://github.com/seleznevae/lua-lua/actions?query=workflow%3ACI+branch%3Amaster) 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- src/lorem.lua
+++ src/lorem.lua
@@ -12,14 +12,14 @@ local BASE_PHRASE = [[
 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 Suspendisse rutrum accumsan elit vel auctor.
 Praesent sit amet aliquam turpis.
-Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ultricies dignissim orci, sed imperdiet dui dignissim ut.
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
 Maecenas non erat ut elit sodales commodo.
 Nam gravida ipsum quis nulla tempus, quis pulvinar augue tristique.
 Nulla massa odio, imperdiet non ultricies tincidunt, viverra sed lorem.
 Nulla elementum sapien ut commodo aliquet.
 Pellentesque iaculis turpis tellus, eget laoreet augue condimentum vel.
 Quisque at risus rhoncus, facilisis tellus nec, tristique dolor.
-Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas cursus magna eget imperdiet laoreet.
+Maecenas cursus magna eget imperdiet laoreet.
 ]]
 
 local function get_words(phrase)
